50c2972ad7aeb86c019ad5023f322b7fd0e49229
Serotoninchik <160415794+Serotoninchik@users.noreply.github.com>
2024-04-15 17:40:44 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,31 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+
+ private:
+    T *arr;
+    int top;
+
+ public:
+    TStack(): top(-1) {
+        arr = new T[size];
+    }
+    T get() const {
+    return arr[top];
+    }
+    bool isEmpty() const {
+    return top == -1;
+    }
+    bool isFull() const {
+    return top == size - 1;
+    }
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+    void push(T item) {
+        if (top < size - 1)
+            arr[++top] = item;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif 
