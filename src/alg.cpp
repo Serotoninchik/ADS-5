0eddc6e81f7cb8517d1fe87d7da5bcf1c489722b
Serotoninchik <160415794+Serotoninchik@users.noreply.github.com>
2024-04-15 17:54:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,101 +1,123 @@
 #include <map>
 #include "tstack.h"
 
-bool isDigit(char ind) {
-return (ind >= '0' && ind <= '9');
-}
-
-bool isOperator(char ind) {
-return (ind == '+' || ind == '-' || ind == '*' || ind == '/'  || ind == '(' || ind == ')');
-}
-
-int prioritet(char ind) {
-    if (ind == '+' || ind == '-')
-    return 1;
-    if (ind == '*' || ind == '/')
-    return 2;
-return 0;
-}
-
+TStack<char, 100> stackf;
+TStack<int, 100> stacks;
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-    std::string post;
-    int count = 0;
-    TStack <char, 100> stack;
-    for (char c : inf) {
-        if (isDigit(c)) {
-            count++;
-            if (count == 1) {
-                post += c;
-                continue;
+    std::string out;
+    for (char ind : inf) {
+        if (ind == 40) {
+            stackf.push(ind);
+        } else if (ind >= 48 && ind <= 57) {
+            out += ind;
+            out += 32;
+        } else if (ind == 41) {
+            while (stackf.get() != 40 && !stackf.isEmpty()) {
+                out += stackf.pop();
+                out += 32;
             }
-            post = post + ' ' + c;
-        } else if (isOperator(c)) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (stack.isEmpty()) {
-                stack.push(c);
-            } else if (prioritet(c) > prioritet(stack.get())) {
-                stack.push(c);
-            } else if (c == ')') {
-                while (stack.get() != '(') {
-                    post = post + ' ' + stack.get();
-                    stack.pop();
+            if (stackf.get() == 40)
+                stackf.pop();
+        } else if (ind == 43 || ind == 45) {
+            if (!stackf.isEmpty()) {
+                switch (stackf.get()) {
+                case 43: {
+                    out += 43;
+                    out += 32;
+                    stackf.pop();
+                    break;
                 }
-                stack.pop();
+                case 45: {
+                    out += 45;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 42: {
+                    out += 42;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 47: {
+                    out += 47;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                }
+                stackf.push(ind);
             } else {
-                int a = prioritet(c);
-                int b = prioritet(stack.get());
-                while (!stack.isEmpty() && a <= b) {
-                    post = post + ' ' + stack.get();
-                    stack.pop();
+                stackf.push(ind);
+            }
+        } else if (ind == 42 || ind == 47) {
+            if (!stackf.isEmpty()) {
+                switch (stackf.get()) {
+                case 42: {
+                    out += 42;
+                    out += 32;
+                    stackf.pop();
+                    break;
                 }
-                stack.push(c);
+                case 47: {
+                    out += 47;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                }
+                stackf.push(ind);
+            } else {
+                stackf.push(ind);
             }
         }
     }
-    while (!stack.isEmpty()) {
-        post = post + ' ' + stack.get();
-        stack.pop();
+    if (!stackf.isEmpty()) {
+        while (!stackf.isEmpty()) {
+            out += stackf.pop();
+            out += 32;
+        }
+        out.pop_back();
     }
-return post;
+    return out;
 }
 
-
-
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
-    TStack <int, 100> stack;
-    for (char c : pref) {
-        if (isDigit(c)) {
-            stack.push(c - '0');
-        } else if (isOperator(c)) {
-            int a = stack.get();
-            stack.pop();
-            int b = stack.get();
-            stack.pop();
-            switch (c) {
-                case '+':
-                    stack.push(a + b);
-                    break;
-                case '-':
-                    stack.push(b - a);
-                    break;
-                case '*':
-                    stack.push(a * b);
-                    break;
-                case '/':
-                    stack.push(b / a);
-                    break;
-                default:
-                    continue;
+    std::string str;
+    char St;
+    for (char ind : pref) {
+        if ((ind >= 48 && ind <= 57)) {
+            str += ind;
+
+        } else if (ind == 32 && !str.empty()) {
+            stacks.push(std::stoi(str));
+            str.clear();
+        } else if (ind == 43 || ind == 45 ||
+            ind == 42 || ind == 47) {
+            switch (ind) {
+            case 43: {
+                St = stacks.pop();
+                stacks.push(stacks.pop() + St);
+                break;
             }
-        } else {
-            continue;
+            case 45: {
+                St = stacks.pop();
+                stacks.push(stacks.pop() - St);
+                break;
+            }
+            case 42: {
+                St = stacks.pop();
+                stacks.push(stacks.pop() * St);
+                break;
+            }
+            case 47: {
+                St = stacks.pop();
+                stacks.push(stacks.pop() / St);
+                break;
+            }
+            }
         }
-        }
-return stack.get();
+    }
+    return stacks.pop();
 }
